02534d3ae9445d5ad1dff917a0400bfb15d6ef58
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2024-11-15 12:18:53 -0500

    a

--- app/main.js
+++ app/main.js
@@ -1,1 +1,18 @@
 import "./style.css";
+async function getData() {
+  try {
+    const response = await fetch(
+      "https://coinlib.io/api/v1/global?key=XXX&pref=EUR"
+    );
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      document.querySelector("h1").textContent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+    alert("sorry could not find that pocket monster");
+  }
+}
+getData();
