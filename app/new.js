697d6c36a56cd966e06c5e04be845d9d5b6df2cd
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2024-12-02 19:20:40 -0500

    a

--- app/new.js
+++ app/new.js
@@ -6,38 +6,36 @@ const DOMselectors = {
 };
 
 async function getTeamRoster(teamId) {
-    const URL = ROSTER_URL.replace("{teamId}", teamId);
-    try {
-      const response = await fetch(URL);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      const players = data.roster;
-  
-      console.log(`Players for team ID ${teamId}:`);
-      players.forEach((player) => {
-        console.log(player.person.fullName); 
-  
-        const cardHTML = createCard(player);
-        const container = document.querySelector(".container");
-        container.innerHTML += cardHTML; 
-      });
-  
-      console.log("Full roster data:", players);
-    } catch (error) {
-      console.log("Error fetching team roster:", error);
+  const URL = ROSTER_URL.replace("{teamId}", teamId);
+  try {
+    const response = await fetch(URL);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+    const data = await response.json();
+    const players = data.roster;
+
+    DOMselectors.container.innerHTML = "";
+
+    console.log(`Players for team ID ${teamId}:`);
+    players.forEach((player) => {
+      console.log(player.person.fullName);
+
+      const cardHTML = createCard(player);
+      DOMselectors.container.innerHTML += cardHTML;
+    });
+
+    console.log("Full roster data:", players);
+  } catch (error) {
+    console.log("Error fetching team roster:", error);
   }
-  
+}
 
 function createCard(player) {
   return `
       <div class="card card-compact bg-base-100 w-96 shadow-xl flex items-center justify-center text-center h-full">
         <h2 class="text-2xl">${player.person.fullName || "Unknown Player"}</h2>
-        <img class="images" src="https://img.mlbstatic.com/mlb-photos/image/upload/w_250,q_auto:best/v1/people/${
-          player.person.id
-        }/headshot/67" 
+        <img class="images" src=${player.person.picture} 
           alt="${player.person.fullName || "Unknown"}" />
         <h3 class="text-lg">Jersey Number: ${player.jerseyNumber || "N/A"}</h3>
         <p class="text-sm">Position: ${player.position.name || "N/A"}</p>
@@ -46,4 +44,9 @@ function createCard(player) {
     `;
 }
 
-getTeamRoster(111);
+document.getElementById("team-select").addEventListener("change", function () {
+  const selectedTeamId = this.value;
+  getTeamRoster(selectedTeamId);
+  console.clear();
+  console.log(`Team selected: ${selectedTeamId}`);
+});
